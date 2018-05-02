833df49ab9c5d7bab2225ea4f748c8fd230ef75f
ChaohuiYu <yu.ch.richard@gmail.com>
2018-05-02 22:03:54 +0800

    [V1] First thoughts
    runtime beats 80.20 % of cpp submissions.

---
+++ 100_Same_Tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+// Obviously, BFS->use recursuve 
+// Come up with the stop condition
+// Be aware the way which you try to track the bool-type data "&&"
+
+
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+    
+        if (p==NULL and q==NULL)
+            return true;
+        
+        if ((!p and q) or (p and !q))
+            return false; 
+        
+        return (p->val==q->val) && (isSameTree(p->left, q->left)) && (isSameTree(p->right,q->right));
+      
+    }
+};
