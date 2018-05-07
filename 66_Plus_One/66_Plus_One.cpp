1e9b9457b361125cebfacbae2437168e777c3a82
chyu <u.65l4cjo@gmail.com>
2018-05-07 17:02:05 +0800

    [20180507] v1

--- 66_Plus_One/66_Plus_One.cpp
+++ 66_Plus_One/66_Plus_One.cpp
@@ -1,41 +1,26 @@
-[1,2,3] 
-123 + 1
-out: [1,2,4]
-[9]
-[1,0]
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+       
+        bool carry = true;
+        int i = digits.size()-1; 
+        
+        while (i >=0)
+        {
+            if (carry) { ++digits[i]; carry = false;} 
+            if (digits[i]>9) { digits[i]%=10; carry=true;}
+            --i;
+        }
+      
+        if (carry)  digits.insert(digits.begin(),1);
+        return digits;
+        
+    }
+};
 
-[9]
-[9]
-[1,0]
 
-"9" + "1"
-  
+// Note
 
-[1,2,3] ->[1, 2, 4]
-
-"9" i +1
-//hint: treat carry as the given 1 for summing up 
-
-#66. Plus One
-//my first thought
-//bool carry = (digits[digits.end()]+1 >10) //<- bool carry = false; <- change it
-vector<int> res;
-bool carry = true;
-//{
-for (it=digits.end()-1:begin ) //<-
-{
-  
-  if (carry)
-  { 
-    *it++;
-    //check_if_carry(*it); // return carry; <-
-  }
-  //else //<-
-  //{
-  	  //check_if_carry(*it);// carry= true; *it=*it%10; [0] <-
-  //}
-  check_if_carry(*it);//here
-}
 // c: true
 // n1: [9] 
 // index i = n1.length - 1
@@ -58,11 +43,3 @@ for (it=digits.end()-1:begin ) //<-
   //res[1:n-1] = digits;
 //}
 //}
-
-
-class Solution {
-public:
-    vector<int> plusOne(vector<int>& digits) {
-        
-    }
-};
