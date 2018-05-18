79aa93df4e3b4245d6596ca47da1cd10c85cb569
chyu <u.65l4cjo@gmail.com>
2018-05-19 01:05:43 +0800

    [20180518]

---
+++ 599_Minimum_Index_Sum_of_Two_Lists/599.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        unordered_map<string, int> map;
+        int least_one = INT_MAX; vector<string> restaurant;
+        for (int i=0;i<list1.size();++i)     map[list1[i]]+=i+1; 
+        for (int i=0;i<list2.size();++i)   
+            if(map[list2[i]]>0)
+            {
+                map[list2[i]]+=i;
+                if (map[list2[i]]<least_one) { least_one=map[list2[i]]; 
+                                              restaurant.resize(0);restaurant.push_back(list2[i]);}
+                else if (map[list2[i]]==least_one){restaurant.push_back(list2[i]);}
+            }
+        return restaurant;  
+    }
+};
