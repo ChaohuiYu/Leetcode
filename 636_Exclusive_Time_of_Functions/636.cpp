42490d4279bc2306750afa38fb752a649bf33e27
chyu <u.65l4cjo@gmail.com>
2018-07-14 19:07:13 +0800

    [20180714 SAT] Stack Problems

---
+++ 636_Exclusive_Time_of_Functions/636.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+        vector<int> result(n);
+        stack<int> s;
+        char typestr[10];
+        int fnid, time, last_time = 0;
+        for ( string & log : logs ) {
+            sscanf(log.c_str(), "%d:%[^:]:%d", &fnid, typestr, &time);
+            if ( 's' == typestr[0] ) {
+                if ( s.size() > 0 )
+                    result[s.top()] += time - last_time;
+                s.push(fnid);
+            } else {
+                result[s.top()] += ++time - last_time;
+                s.pop();
+            }
+            last_time = time;
+        }
+        return result;
+    }
+};
