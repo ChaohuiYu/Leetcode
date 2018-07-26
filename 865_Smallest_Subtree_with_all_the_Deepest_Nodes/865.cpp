cc7c3b316c302f3a4673d07f37da338cc7bfb132
chyu <u.65l4cjo@gmail.com>
2018-07-27 00:00:31 +0800

    [20180727 THU] TREE Problems

--- 865_Smallest_Subtree_with_all_the_Deepest_Nodes/865.cpp
+++ 865_Smallest_Subtree_with_all_the_Deepest_Nodes/865.cpp
@@ -1,3 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        TreeNode* tar = root;
+        
+        int max_depth = INT_MIN;
+        int cur_depth = 1;
+        subtreeHelper(root, cur_depth, max_depth, tar );
+        return tar;
+    }
+    
+    int subtreeHelper(TreeNode* n , int depth, int& max_level, TreeNode* &tar)
+    {
+        if (!n)
+            return depth-1;
+        int l_depth = subtreeHelper(n->left, depth+1, max_level, tar);
+        int r_depth = subtreeHelper(n->right, depth+1, max_level, tar);
+        
+        if (depth > max_level)
+        {
+            tar = n;
+            max_level = depth;
+        }
+        else if (l_depth==r_depth and l_depth==max_level)
+        {
+            tar = n;
+        }
+        
+        return max(l_depth, r_depth);
+        
+    }
+    
+};
+
 class Solution {
 public:
     int utils(TreeNode *root, int curdepth, int &maxdepth, TreeNode* &curCand) {
