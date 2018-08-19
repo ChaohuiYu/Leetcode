c2b822fc7db286b1aa0c8d9b4b2975c3d7395574
chyu <u.65l4cjo@gmail.com>
2018-08-19 23:42:28 +0800

    [20180819 SUN] DP Problems

--- 062_Unique_Paths/62.cpp
+++ 062_Unique_Paths/62.cpp
@@ -1,4 +1,22 @@
 class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        
+        int n = cost.size();
+        vector<int> min_cost(n, 0);
+        
+        min_cost[0] = cost[0];
+        min_cost[1] = cost[1];
+        for (int i = 2; i < n; ++i)
+            min_cost[i] = min(min_cost[i-1], min_cost[i-2]) + cost[i];
+        
+        return min(min_cost[n-1], min_cost[n-2]);
+    }
+};
+
+
+
+class Solution {
     
 public:
     int uniquePaths(int m, int n) {
