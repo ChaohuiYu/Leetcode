6286f35c8e6c940d982693933f46d66fcb17a7b2
chyu <u.65l4cjo@gmail.com>
2018-08-29 09:42:36 +0800

    [20180828 TUE] DP Problems

--- 121_Best_Time_to_Buy_and_Sell_Stock/121.cpp
+++ 121_Best_Time_to_Buy_and_Sell_Stock/121.cpp
@@ -1,1 +1,13 @@
-i
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int ans = 0, low = INT_MAX;
+        for (auto i:prices){
+            ans = max(i - low, ans);
+            if (i < low) low = i;
+        }
+        return ans;
+    }
+};
